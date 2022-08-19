5e75ecc9e38194aad81e8620ff4ea08b9d5d5e5e
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-08-19 21:37:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    let newArr = [];
+    for (let t = 0; t < 2; t++) {
+      newArr.push(arr[t]);
+    }
+    return newArr;
+  }
+  
+  function returnLastTwoDrivers(arr) {
+    let newArr = [];
+    for (let t = arr.length - 2; t < arr.length; t++) {
+      newArr.push(arr[t]);
+    }
+    return newArr;
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(fare) {
+    return () => {
+      return fare * fare;
+    }
+  }
+  
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+  
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  
+  function selectDifferentDrivers(arrayOfDrivers, f) {
+    return f(arrayOfDrivers);
+  }
